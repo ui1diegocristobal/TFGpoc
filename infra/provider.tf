8bac72c6e1cf2f140f9e81e03ce9bad9de2451ed
Diego Cristobal Herreros <diego.cristobal@alumnos.ui1.es>
2022-05-07 16:13:48 +0200

    Feat: add s3 backend

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,5 +1,10 @@
 # Specify required provider as maintained by civo
 terraform {
+  backend "s3" {
+    bucket = "tfg-diego-s3"
+    key    = "tfg-diego-s3/tfstate"
+    region = "us-east-1"
+  }
   required_providers {
     civo = {
       source = "civo/civo"
