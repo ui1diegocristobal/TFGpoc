02e9f537e8648a39b2f841824eba7b328af39f4c
Diego Cristobal Herreros <diego.cristobal@alumnos.ui1.es>
2022-06-04 06:18:42 +0200

    Feat: add kubeconfig templating to flux and apps for deploying

---
+++ scripts/kubeconfig.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+read -r -d '' KUBECONFIG <<EOF
+apiVersion: v1
+clusters:
+- cluster:
+    certificate-authority-data: $CERT_DATA
+    server: https://74.220.19.194:6443
+  name: cluster_tf
+contexts:
+- context:
+    cluster: cluster_tf
+    user: cluster_tf
+  name: cluster_tf
+current-context: cluster_tf
+kind: Config
+preferences: {}
+users:
+- name: cluster_tf
+  user:
+    client-certificate-data: $CERT_DATA
+    client-key-data: $KEY_DATA
+EOF
+echo "${KUBECONFIG}" > ~/.kube/config
